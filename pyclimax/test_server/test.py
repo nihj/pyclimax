422f8d9bdaa1d4e4abf3ffaf08411b56b6fe3858
Niklas Hjern <niklashj@axis.com>
2018-12-27 22:18:55 +0100

    Add test file using flask server
    Change-Id: I67a023092d7a72499f397e13ad6e4d183f2135e3

---
+++ pyclimax/test_server/test.py
@@ -0,0 +1,22 @@
+import requests
+import time
+import climax as ClimaxApi
+
+
+def callback(device):
+    print('Got callback from device: %s', device.name)
+
+[controller, created] =  ClimaxApi.init_controller('http://127.0.0.1:5000', '', '')
+
+niklas_switch = controller.get_device_by_id('ZB:0000000000006b07')
+
+print(niklas_switch.json_state)
+
+controller.start()
+
+controller.register(niklas_switch, callback)
+
+while 1:
+    time.sleep(2)
+
+controller.stop()
